60934455453841d275d71d0551251bf2c200307b
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-03-03 02:09:29 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -276,10 +276,10 @@ export default function App() {
     <Router>
     <Header />
       <Routes>
-      <Route path="/" exact  element={<Home />} ></Route>
-      <Route path="/contact" exact  element={<Contact />}></Route>
-      <Route path="/about" exact  element={<About />}></Route>
-      <Route path="/links" exact  element={<Links />}></Route>
+      <Route path="manogawra/" exact  element={<Home />} ></Route>
+      <Route path="manogawra/contact" exact  element={<Contact />}></Route>
+      <Route path="manogawra/about" exact  element={<About />}></Route>
+      <Route path="manogawra/links" exact  element={<Links />}></Route>
       <Route
             path="/gallery"
             exact
