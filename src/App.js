c59e1dbb0398b978cf7f8a96a4ecdbfdfc2a0faf
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-03-03 02:14:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router , Route , Routes } from "react-router-dom";
+import { HashRouter as Router , Route , Routes } from "react-router-dom";
 import Contact from "./pages/Contact"
 import Links from "./pages/Links"
 import Header from "./components/Header"
