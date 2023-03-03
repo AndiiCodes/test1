92035acb9aabf97cfede121a3e6c3151324d2ffc
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-03-03 15:57:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter as Router2 } from 'react-router-dom';
+
 
 import App from './App';
 
