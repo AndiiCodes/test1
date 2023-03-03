9190d35653b1be5713b79052c7488fbaabb9c66f
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-03-03 14:29:51 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router2 } from 'react-router-dom';
 
 import App from './App';
-import { HashRouter as Router } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-  <Router>
-    <App />
-  </Router>
-=======
- <Router>
 
+  <Router2>
     <App />
-  
- </Router>
->>>>>>> 54b960c3cc7e622552b9de736e3bc11971270f09
+  </Router2>
+
+
 );
