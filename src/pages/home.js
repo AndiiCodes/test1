ef98ebe011ddde15ab0dd07a4b4e23ac328749ab
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-03-17 18:28:30 +0300

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -93,21 +93,20 @@ export default function Home() {
             src="https://i.ibb.co/GdjGqS9/p.jpg"
             alt=""
           />
-        </span>
-        <h1
-          className="lg:relative text-white sm:flex sm:justify-center lg:text-[3.5rem] lg:bottom-[30rem] lg:left-[10%] xl:bottom-[35rem] xl:left-[10%] left-24 lg:py-0 font-bold py-8 text-center text-3xl lg:leading-[3rem] 2xl:static 2xl:text-7xl 2xl:mt-[2rem]"
-        >
-          <span>
-     Photography&nbsp;<br className="hidden lg:block" />helps&nbsp;people<br className="hidden lg:block" />to&nbsp;see.
-  </span>
-        </h1>
+          <h1
+            className="lg:relative text-white sm:flex sm:justify-center lg:text-[3.5rem] lg:bottom-[30rem] lg:left-[10%] xl:bottom-[35rem] xl:left-[10%] left-24 lg:py-0 font-bold py-8 text-center text-3xl lg:leading-[3rem] 2xl:static 2xl:text-7xl 2xl:mt-[2rem]"
+          >
+            <span>
+              Photography&nbsp;<br className="hidden lg:block" />helps&nbsp;people<br className="hidden lg:block" />to&nbsp;see.
+            </span>
+          </h1>
           <p className="text-white xl:text-xl xl:relative xl:left-[40rem] font-['os'] xl:-top-[30rem] lg:relative lg:left-[30rem] lg:-top-[25rem] sm:block lg:text-justify sm:relative sm:text-center text-center sm:left-auto font-light break-words 2xl:static 2xl:text-center 2xl:mt-[5rem] 2xl:text-3xl">
-            From inspiring people's stories to impactful messages,<br class="hidden md:block" />
-I create head-turning photographs that do the<br class="hidden md:block" />
-right thing, in the right place, at the right<br class="hidden md:block" />
-time to unlock the possibility.
+            From inspiring people's stories to impactful messages,<br className="hidden md:block" />
+            I create head-turning photographs that do the<br className="hidden md:block" />
+            right thing, in the right place, at the right<br className="hidden md:block" />
+            time to unlock the possibility.
           </p>
-
+        </span>
       </div>
       <Gallery />
     </div>
