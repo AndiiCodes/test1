21cbe3e99d1e40721d13f19307017a7f875ab107
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-03-23 01:54:57 +0300

    Update Links.js

--- src/pages/Links.js
+++ src/pages/Links.js
@@ -155,7 +155,7 @@ export default function Links() {
         </div>
         <div className='flex flex-col justify-center items-center py-5'>
           <div className='w-full max-w-[25rem] border-l-4 border-white text-white bg-[#214029] p-5 flex'>
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"/></svg>
+<svg className='px-[20px]' fill='white' width={100} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"/></svg>
             <div className='flex flex-col'>
               <a href='https://www.facebook.com/Manuel.Gawra' className='text-lg font-bold underline hover:text-orange-400'>
                 <h1>Facebook</h1>
