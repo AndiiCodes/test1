2e32cc9715b9113ef98919a0055eda2c29c7a773
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-03-28 18:34:52 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
         <Navbar
   fluid={true}
   rounded={true}
-  className="bg sm:bg-[#132623]"
+  className="bg sm:bg-[#ffffff]"
 >
   <Navbar.Brand >
     <Link to="/">
